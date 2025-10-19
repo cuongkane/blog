9da753855457d202a8ec8920fa67f13c16916697
lexuancuong <cuongle.software@gmail.com>
2025-10-19 23:11:03 +0700

    Migrate to Next JS part 1: Setup the repo using to use NextJS and Mdx

--- src/components/layout/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer: React.FC = () => {
-  return (
-    <div className="footer">
-      <div>Xuan-Cuong Le(Kane)</div>
-    </div>
-  );
-};
-
-export default Footer;
---
+++ src/config/site.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+  name: "cuongkane",
+  url: "https://cuongkane.com",
+  description:
+    "Personal blog about software engineering, programming experiences, and technology insights.",
+  author: "cuongkane",
+  social: {
+    github: "https://github.com/cuongkane",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
